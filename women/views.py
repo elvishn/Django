ad6e26724327fc4f3c1ce39d34e7ea7d0a0f3389
Elene <elvishn475@gmail.com>
2024-11-28 20:26:23 +0300

    Пагинация и функция

--- women/views.py
+++ women/views.py
@@ -1,5 +1,6 @@
 import uuid
 from PIL import Image
+from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse, reverse_lazy
@@ -40,15 +41,12 @@ class WomenCategory(DataMixin, ListView):
         return Women.published.filter(cat__slug=self.kwargs['cat_slug']).select_related('cat')
 
 def about(request, form_cleaned_data=None):
-    if request.method == "POST":
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            fp = UploadFiles(file=form_cleaned_data['file'])
-            fp.save()
-    else:
-        form = UploadFileForm()
+    contact_list = Women.published.all()
+    paginator = Paginator(contact_list, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
-    return render(request, 'women/about.html', {'title': 'О сайте', 'form': form})
+    return render(request, 'women/about.html', {'title': 'О сайте', 'page_obj': page_obj})
 
 class ShowPost(DataMixin, DetailView):
     model = Women
