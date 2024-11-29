40682e3150aa2a81ca6d660e1d2ea2d7202bfe3f
Elene <elvishn475@gmail.com>
2024-11-29 15:39:22 +0300

    Добавление пагинации через классы

--- women/utils.py
+++ women/utils.py
@@ -7,7 +7,7 @@ class DataMixin:
     title_page = None
     extra_context = {}
     cat_selected = None
-
+    paginate_by = 2
     def __init__(self):
         if self.title_page:
             self.extra_context['title'] = self.title_page
