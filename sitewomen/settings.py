f1bd4d092a85224714284cb8057490cd3be5aa65
Elene <elvishn475@gmail.com>
2024-12-03 20:26:25 +0300

    Добавлена авторизация по email

--- sitewomen/settings.py
+++ sitewomen/settings.py
@@ -140,3 +140,7 @@ MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 LOGIN_URL = 'users:login'
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'users.authentication.EmailAuthBackend'
+]
