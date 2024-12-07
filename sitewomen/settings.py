a8a6c9cc4287ba9008d9f8a3eb02d5d3b744d988
Elene <elvishn475@gmail.com>
2024-12-07 18:34:00 +0300

    Настройка почтового сервера по SMTP-протоколу

--- sitewomen/settings.py
+++ sitewomen/settings.py
@@ -144,4 +144,12 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'users.authentication.EmailAuthBackend',
 ]
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST_PASSWORD = 'lzswiumijlnazfjs'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'elvisnh@yandex.ru'
+EMAIL_USE_SSL = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
